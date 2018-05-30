db1324499478d3fa34a6c60ccc6e5ff542e38a39
ErinLMoore <emoore@pillartechnology.com>
2018-05-30 16:50:22 -0400

    SMRT-33 added npm package to mock websockets
    co-authored-by: Anirudh Ganesh <theanig@protonmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { WebSocket } from 'mock-socket'
+
+global.WebSocket = WebSocket
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
