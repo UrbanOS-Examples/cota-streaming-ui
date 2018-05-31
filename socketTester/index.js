f61f1c37c025509478a5922327e370ecd938eb1c
Jessie Morris <jmorris2@pillartechnology.com>
2018-05-31 10:37:27 -0400

    Fixing the socket tester to not fail when the front end closes

--- socketTester/index.js
+++ socketTester/index.js
@@ -5,12 +5,18 @@ var app = express()
 
 const wss = new WebSocket.Server({ port: 8080});
 
-wss.on('connection', function connection(ws) {
-	setInterval(function() {
-		ws.send("Hey");
-	}, 3000);
-});
+wss.on('connection', function connection(ws) {});
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+setInterval(function() {
+  wss.broadcast("Hey");
+}, 3000);
 
 app.listen(3001, function () {
   console.log('Testing sockets')
