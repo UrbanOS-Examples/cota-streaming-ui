17d87fc8a648c1a12df109739d38fb2d5f49f9ac
Jessie Morris <jmorris2@pillartechnology.com>
2018-05-31 16:06:03 -0400

    Hopefully sending accross a json payload

--- socketTester/index.js
+++ socketTester/index.js
@@ -5,6 +5,41 @@ var app = express()
 
 const wss = new WebSocket.Server({ port: 8080});
 
+someJson = {
+    "vehicle": {
+        "vehicle": {
+            "license_plate": null,
+            "label": "2939",
+            "id": "12939"
+        },
+        "trip": {
+            "trip_id": "637274",
+            "start_time": null,
+            "start_date": "20180531",
+            "schedule_relationship": null,
+            "route_id": "152",
+            "direction_id": null
+        },
+        "timestamp": 1527795212,
+        "stop_id": null,
+        "position": {
+            "speed": 8.630155434730113e-7,
+            "odometer": null,
+            "longitude": -82.885498046875,
+            "latitude": 39.99894714355469,
+            "bearing": 270.0
+        },
+        "occupancy_status": null,
+        "current_stop_sequence": null,
+        "current_status": "IN_TRANSIT_TO",
+        "congestion_level": null
+    },
+    "trip_update": null,
+    "is_deleted": false,
+    "id": "2939",
+    "alert": null
+};
+
 wss.on('connection', function connection(ws) {});
 
 wss.broadcast = function broadcast(data) {
@@ -15,7 +50,7 @@ wss.broadcast = function broadcast(data) {
   });
 };
 setInterval(function() {
-  wss.broadcast("Hey");
+  wss.broadcast(someJson);
 }, 3000);
 
 app.listen(3001, function () {
