3f9af1a20aa7c45b8643d895390c4aab5752d277
Jeffrey Lutz <jeff.lutz@agilenanswers.com>
2018-06-20 14:12:56 -0400

    SMRT-114 Created table under map
    co-authored-by: Brandon Cromer <bcromer@pillartechnology.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import COTAPositionMap from './components/cota-position-map'
+import DataTable from './components/data-table'
 import Header from './components/header'
 
 import './App.scss'
@@ -7,6 +8,9 @@ import './App.scss'
 export default () => (
   <main-app-element>
     <Header />
-    <COTAPositionMap />
+    <div className='main-content'>
+      <COTAPositionMap />
+      <DataTable />
+    </div>
   </main-app-element>
 )
---
+++ src/higher-order-components/with-websocket.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import lodash from 'lodash'
+import { Socket } from 'phoenix'
+
+export default function withStream (WrappedComponent, websocketURL) {
+  return class extends React.Component {
+    constructor (props) {
+      super(props)
+      this.websocketURL = websocketURL
+      this.state = {streamData: []}
+    }
+
+    componentDidMount () {
+      this.socket = new Socket(this.websocketURL)
+      this.socket.connect()
+
+      // vehicle_position is the topic that is subscribed to
+      this.channel = this.socket.channel('vehicle_position')
+      // The update event is the event on the message that Phoenix sends
+      this.channel.on('update', msg => this.onMessageArrived(msg))
+
+      this.channel.join()
+        .receive('ok', () => console.log('Connection Successful'))
+        .receive('error', ({reason}) => console.log('failed join', reason))
+        .receive('timeout', () => console.log('Networking issue. Still waiting...'))
+    }
+
+    onSocketOpen (websocket) {
+      websocket.send('{"topic":"vehicle_position","event":"phx_join","payload":{},"ref":"1"}')
+    }
+
+    onMessageArrived (message) {
+      let clonedStreamData = this.state.streamData.slice(0)
+
+      clonedStreamData.unshift(this.parseCOTAJSON(message))
+
+      this.setState({ streamData: lodash.take(clonedStreamData, 100) })
+    }
+
+    parseCOTAJSON (json) {
+      const vehicleData = json.vehicle
+      return {
+        'vehicleId': vehicleData.vehicle.id,
+        'routeId': vehicleData.trip.route_id,
+        'latitude': vehicleData.position.latitude,
+        'longitude': vehicleData.position.longitude,
+        'timestamp': vehicleData.timestamp * 1000
+      }
+    }
+
+    render () {
+      return <WrappedComponent data={this.state.streamData} {...this.props} />
+    }
+  }
+}
