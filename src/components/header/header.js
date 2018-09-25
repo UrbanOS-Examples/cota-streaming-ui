bb2a1a3c02fd0eae5292029104441092169b27e7
Brandon Cromer <bcromer07@gmail.com>
2018-09-25 14:37:49 -0400

    Updated COTA logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import scosLogo from '../../assets/smrt-columbus.png'
+import cotaLogo from '../../assets/COTA-logo.svg'
 import './header.scss'
 
 export default () => (
   <app-header>
     <div className='logos'>
-      <img src={scosLogo} className='cota-logo' alt='logo' />
+      <img src={scosLogo} className='app-logo' alt='logo' />
       <div className='conjunction-junction'>+</div>
-      <img src={'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Cota-logo.svg/1280px-Cota-logo.svg.png'} className='app-logo' alt='logo' />
+      <img src={cotaLogo} className='cota-logo' alt='logo' />
     </div>
     <a className='about-map-button' href='#'>About This Map</a>
   </app-header>
