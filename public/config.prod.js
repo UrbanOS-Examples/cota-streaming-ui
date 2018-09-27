c46bad57e4ace0db3fd5d435ac591c42043ef4a0
Brandon Cromer <bcromer07@gmail.com>
2018-09-27 13:52:44 -0400

    SMRT-549 Changed the dataset url to just be a host for Joomla
    co-authored-by: Scott Millard <smillard@hntb.com>

--- public/config.prod.js
+++ public/config.prod.js
@@ -1,2 +1,2 @@
 window.WEBSOCKET_HOST = 'wss://streaming.smartcolumbusos.com'
-window.DATA_LOCATION = 'https://www.smartcolumbusos.com/data-stories/streaming-data-feed-shows-riders-where-their-bus-is-in-real-time'
+window.JOOMLA_HOST = 'https://www.smartcolumbusos.com'
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,8 @@ import scosLogo from '../../assets/smrt-columbus.png'
 import cotaLogo from '../../assets/COTA-logo.svg'
 import './header.scss'
 
+const DATA_FEED_PATH = "/data-stories/streaming-data-feed-shows-riders-where-their-bus-is-in-real-time"
+
 export default () => (
   <app-header>
     <div className='logos'>
@@ -10,6 +12,6 @@ export default () => (
       <div className='conjunction-junction'>+</div>
       <img src={cotaLogo} className='cota-logo' alt='logo' />
     </div>
-    <a className='about-map-button' target='_blank' href={window.DATA_LOCATION}>About This Map</a>
+    <a className='about-map-button' target='_blank' href={window.JOOMLA_HOST + DATA_FEED_PATH}>About This Map</a>
   </app-header>
 )
