48c2f633f134f78280035b521d9cbf3494d2995d
bbalser <bbalser@pillartechnology.com>
2019-05-07 09:52:38 -0400

    Change dns names of streams websocket

--- public/config.dev.js
+++ public/config.dev.js
@@ -1,2 +1,2 @@
-window.WEBSOCKET_HOST = 'wss://socket.dev.internal.smartcolumbusos.com'
+window.WEBSOCKET_HOST = 'wss://streams.dev.internal.smartcolumbusos.com'
 window.JOOMLA_HOST = 'https://www.dev.internal.smartcolumbusos.com'
