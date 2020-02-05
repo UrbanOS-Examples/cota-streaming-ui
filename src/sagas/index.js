5289c1ccbc56e11946c6cc883372f6586311ec2d
mcsearchin <abrock17@gmail.com>
2020-02-05 15:55:18 -0500

    DRYd up gee 2 web socket sagas
    smartcitiesdata/smartcitiesdata#487
    
    co-authored-by: Scott Millard <smillard@hntb.com>

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,11 @@
-import webSocketSaga from './websocket'
+import cotaWebSocketSaga from './cotaWebSocket'
 import leapWebSocketSaga from './leapWebSocket'
 import routeSaga from './route'
 import { fork, all } from 'redux-saga/effects'
 
 export default function* allSagas() {
   yield all([
-    fork(webSocketSaga),
+    fork(cotaWebSocketSaga),
     fork(leapWebSocketSaga),
     fork(routeSaga)
   ])
--- src/sagas/leapWebSocket.js
+++ src/sagas/leapWebSocket.js
@@ -1,43 +1,10 @@
 import { call, take, put, race, select } from 'redux-saga/effects'
-import { eventChannel } from 'redux-saga'
-import { Socket } from 'phoenix'
+import * as socketUtils from './websocket-utils'
 import { leapPositionUpdate, ROUTE_FILTER } from '../actions'
 import { LEAP } from '../variables'
 
-/*
-  socket.onOpen is sending the existing filters when the socket is opened.
-  This solves the issue of the browser sending the original filter on a reconnection.
-  This needs to be global state because socket.onOpen can not take a generator function,
-  which is required to be executed to get data out of the Redux state
- */
-
-export let createSocket = (socketUrl) => {
-  let socket = new Socket(socketUrl)
-  socket.connect()
-  return socket
-}
-
 const createChannel = (socket) => {
   return socket.channel('streaming:easymile__linden_positions', {})
-}
-
-const sendFilter = (channel) => {
-  channel.push('filter', {})
-}
-
-const unsubscribe = () => { }
-
-const createEventChannel = channel => {
-  return eventChannel(emit => {
-    channel.on('update', emit)
-
-    channel.join()
-      .receive('ok', () => console.log('Connection Successful'))
-      .receive('error', ({ reason }) => console.log('failed join', reason))
-      .receive('timeout', () => console.log('Networking issue. Still waiting...'))
-
-    return unsubscribe
-  })
 }
 
 const fromServer = function* (eventChannel) {
@@ -58,15 +25,15 @@ const fromEventBus = function* (channel) {
   while (true) {
     const action = yield take(ROUTE_FILTER)
     if (LEAP === action.filter[0]) {
-      yield call(sendFilter, channel)
+      channel.push('filter', {})
     }
   }
 }
 
 const doSaga = function* () {
-  const socket = yield call(createSocket, `${window.WEBSOCKET_HOST}/socket`)
+  const socket = yield call(socketUtils.createSocket, `${window.WEBSOCKET_HOST}/socket`)
   const channel = yield call(createChannel, socket)
-  const eventChannel = yield call(createEventChannel, channel)
+  const eventChannel = yield call(socketUtils.createEventChannel, channel)
 
   yield race([call(fromEventBus, channel), call(fromServer, eventChannel)])
 }
